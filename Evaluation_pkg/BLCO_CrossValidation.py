55c1fc541ca35b3eb8469701fe5ee41c17f73173
Shen <s.siyuan@compute1-client-1.ris.wustl.edu>
2024-09-02 17:53:37 -0500

    Update BLCO cluster distance calculation loop

--- Evaluation_pkg/BLCO_CrossValidation.py
+++ Evaluation_pkg/BLCO_CrossValidation.py
@@ -433,7 +433,7 @@ def derive_Test_Training_index_4Each_BLCO_fold(kfolds, number_of_SeedClusters, s
         
         # find distances between selected stations and other stations
         sites_unwithheld4testing_Distance = np.zeros((number_of_SeedClusters,len(sites_unwithheld4testing)))
-        for icluster in range(number_of_SeedClusters):
+        for icluster in range(len(cluster_seeds_index)):
             print('icluster: {}, \ncluster_seeds_index shape: {}, \nsites_unwithheld4testing shape:{}, \n site_lat shape:{}; site lon shape: {}, \nsites_unwithheld4testing_Distance shape:{}'.format(icluster,cluster_seeds_index.shape,sites_unwithheld4testing.shape,site_lat.shape,site_lon.shape,sites_unwithheld4testing_Distance.shape))
             temp_distance = calculate_distance_forArray(site_lat=site_lat[sites_unwithheld4testing[cluster_seeds_index[icluster]]],
                                                                                         site_lon=site_lon[sites_unwithheld4testing[cluster_seeds_index[icluster]]],
